f7ba5b776bc0352a25534e8195b53aaf47bea701
Pritesh Anand <anandpritesh.17@gmail.com>
2017-10-20 17:23:09 +0530

    Added default module

--- HRM/grails-app/init/hrm/BootStrap.groovy
+++ HRM/grails-app/init/hrm/BootStrap.groovy
@@ -1,7 +1,9 @@
 package hrm
 
+import com.hrm.Menu
 import com.hrm.Module
 import com.hrm.Role
+import com.hrm.RoleEmployee
 import com.hrm.User
 import com.hrm.UserRole
 import com.invoice.Currency
@@ -16,9 +18,11 @@ class BootStrap {
         def superAdminRole  = new Role(authority: 'ROLE_SUPERADMIN').save()
         def employeeRole  = new Role(authority: 'ROLE_EMPLOYEE').save()
         def managerRole  = new Role(authority: 'ROLE_MANAGER').save()
+      //  def employee_role  = new RoleEmployee(name : "MANAGER_LEVEL_1" ,type: "MANAGER",accessibility : "medium",priority : "7").save()
         def moduleGeo = new Module(moduleName : "GeoZone" ,moduleType : "Bussiness").save()
         def moduleInv = new Module(moduleName : "Invoice" ,moduleType : "Bussiness").save()
         def moduleMar = new Module(moduleName : "Facebook" ,moduleType : "Social").save()
+        def menu = new Menu(name : "Home" ,orderBy : "0").save()
         def currencyInr = new Currency(name: "Indian Rupee", symbol: "INR", country: "India", code: "INR").save()
         def currencyAll = new Currency(name: "Albania Lek", symbol: "Lek", country: "Albania", code: "ALL").save()
         def currencyAfn = new Currency(name: "Afghanistan Afghani", symbol: "؋", country: "Afghanistan", code: "AFN").save()
